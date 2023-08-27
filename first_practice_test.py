e1ee6aa39df66004230dd2124b5df2841eedb8e2
Flora-Lii <liziyang678904@outlook.com>
2023-08-27 10:46:18 +0800

    add files

---
+++ first_practice_test.py
@@ -0,0 +1,9 @@
+number1 = int(input("您输入的第一个数字为："))
+number2 = int(input("您输入的第二个数字为："))
+result = number1 * number2
+sum = number1 + number2
+if result <= 1000:
+    print(result)
+else:
+    print(sum)
+
